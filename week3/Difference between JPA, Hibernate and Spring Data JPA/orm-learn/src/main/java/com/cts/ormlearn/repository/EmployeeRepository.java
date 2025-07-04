767abe72d7ba5b06747aeeec407f0e34714646f9
SujanaArikati <sujana.arikati@gmail.com>
2025-07-04 23:37:15 +0530

    Delete week3 directory

--- week3/Difference between JPA, Hibernate and Spring Data JPA/orm-learn/src/main/java/com/cts/ormlearn/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.cts.ormlearn.repository;
-
-import com.cts.ormlearn.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
--- week3/Difference between JPA, Hibernate and Spring Data JPA/orm-learn/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-
-INSERT INTO employee (name, department, salary) VALUES ('John Doe', 'IT', 60000);
